9495734fdb6140d7a685a65593e6c9ebc3eb9fd8
J. Tanner Porter <jtporter9@gmail.com>
2016-04-19 23:49:07 -0600

    image routes fixed

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ angular.module('portfolio', ['ui.router'])
 		$stateProvider
 			.state('/', {
 				url: '/',
-				templateUrl: 'js/landing/landing.html',
+				templateUrl: 'Public/js/landing/landing.html',
 				controller: 'landingCtrl'
 			})
 
 			.state('admin', {
 				url: '/admin',
-				templateUrl: 'js/admin/admin.html',
+				templateUrl: 'Public/js/admin/admin.html',
 				controller: 'adminCtrl'
 			})
 	});
